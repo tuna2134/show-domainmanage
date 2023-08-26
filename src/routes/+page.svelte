28e01e8669fbe09c0d3a9367662b126f80bc0d6b
mc_fdc <Masato.04.11.2007@gmail.com>
2023-08-26 16:31:44 +0000

    とりあえずアップロード

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let domain = "";
+    onMount(() => domain = window.location.hostname);
+</script>
+
+<svelte:head>
+    <title>tuna2134 managed domain</title>
+</svelte:head>
+<div class="font-mplus1p w-full flex justify-center min-h-screen items-center bg-slate-800 text-white">
+    <div class="px-auto space-y-2">
+        <h1 class="text-4xl font-bold">tuna2134 managed domain</h1>
+        <div class="space-y-2">
+            <div>
+                <p>{domain} is managed by tuna2134.</p>
+                <p>If you need to contact us, please send email.</p>
+            </div>
+            <p>Email: <a class="text-sky-400" href="mailto:me@tuna2134.dev">me@tuna2134.dev</a></p>
+        </div>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus1p: ['"M PLUS 1p"']
+      }
+    },
+  },
+  plugins: [],
+}
+
